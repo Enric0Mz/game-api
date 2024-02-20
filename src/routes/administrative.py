d89cdeff4f92e6934224badd6ceb8b4e78e29c3c
enrico <enrico.marquez@duko.com.br>
2024-02-19 23:17:03 -0300

    feat(game): defining route for creation of game

---
+++ src/routes/administrative.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import Form
+
+from src.database.connection import DbConnectionHandler
+from src.api import dependencies
+from src.domain import game
+from src import schemas
+
+router = APIRouter()
+
+
+@router.post("/game", response_model=schemas.Game)
+async def crete_game(
+    context: DbConnectionHandler = Depends(dependencies.get_database_connection),
+    payload: schemas.Game = Form(...)
+):
+    return await game.CreateGameUseCase(context, payload).execute()
