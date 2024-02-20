a405ef74e58f2adf0ca8a0ad2ef541307c20e0fc
enrico <enrico.marquez@duko.com.br>
2024-02-19 22:46:12 -0300

    feat: defining lifespan on main app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from src.api.events import lifespan
+
 
 def get_application() -> FastAPI:
-    _app = FastAPI(title="Game Api", version="0.1.0", debug=True)
+    _app = FastAPI(title="Game Api", version="0.1.0", debug=True, lifespan=lifespan)
 
     return _app
 
