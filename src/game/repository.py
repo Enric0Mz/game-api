3bc3e8dae4cdd71759a842f202aec8f79dca7cfe
enrico <enrico.marquez@duko.com.br>
2024-02-28 20:00:54 -0300

    feat(game): defining get method in game repository'

--- src/game/repository.py
+++ src/game/repository.py
@@ -19,6 +19,12 @@ class GameRepository(Repository):
     async def fetch(self, clause: QueryExpression):
         result = await self.context.acquire_session().find(GameModel, clause)
         return [self.to_dto(obj) for obj in result]
+    
+    async def get(self, clause: QueryExpression):
+        result = await self.context.acquire_session().find_one(GameModel, clause)
+        if first := result:
+            return self.to_dto(first)
+        raise "NOT FOUND ERROR" #TODO make correct exception        
 
     async def create(self, payload: schemas.CreateGame) -> schemas.Game:
         result = await self.context.acquire_session().save(GameModel(**payload.model_dump()))
