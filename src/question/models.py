66a069ffea55734db44d67e66c0510c457d920e7
enrico <enrico.marquez@duko.com.br>
2024-02-28 15:16:57 -0300

    feat(question): initializing question module and defining models

---
+++ src/question/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from odmantic import Model
+from odmantic import EmbeddedModel
+
+
+class ChoiceModel(Model):
+    name: str
+    correct: bool
+    position: int
+
+
+class QuestyonTypeModelEmbedded(EmbeddedModel):
+    difficulty: str
+    point_multiplier: int
+
+
+class GameModelEmbedded(EmbeddedModel):
+    subject: str
+    created_at: datetime
+    start_at: datetime
+    finish_at: datetime
+
+
+class QuestionModel(Model):
+    name: str
+    choices: list[ChoiceModel]
+    questionType: str
+    game: GameModelEmbedded
+    point_value: int
