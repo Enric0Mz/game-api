a07193d9b6ce7d205097039f60a7825a7a1bf9ac
enrico <enrico.marquez@duko.com.br>
2024-02-28 19:21:50 -0300

    feat(question): defining question schemas

---
+++ src/question/schemas.py
@@ -0,0 +1,21 @@
+from src.api.schema import Schema
+from src.game.schemas import Game
+
+
+class Choice(Schema):
+    name: str
+    correct: bool
+    position: int
+
+
+class QuestyonType(Schema):
+    difficulty: str
+    point_multiplier: int
+
+
+class Question(Schema):
+    name: str
+    choices: list[Choice]
+    questionType: QuestyonType
+    game: Game
+    point_value: int
