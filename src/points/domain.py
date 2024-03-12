741e91c3f235dc3e64cf98b73816f2c0206923de
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-12 20:37:52 -0300

    feat(points) defining list user points use case

---
+++ src/points/domain.py
@@ -0,0 +1,19 @@
+from odmantic import query
+
+from src.database.connection import DbConnectionHandler
+from src.common.page import PaginationParams
+
+from .repository import PointRepository
+from .models import PointModel
+from . import schemas
+
+
+class ListUserPointsUseCase:
+    def __init__(self, context: DbConnectionHandler, params: PaginationParams) -> None:
+        self._repository = PointRepository(context)
+        self._params = params
+
+
+    async def execute(self):
+        return {"data": await self._repository.fetch(None)} # TODO add user query later
+    
