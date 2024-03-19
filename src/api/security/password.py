df55b5efa592c19c513df581f6787ebe91040bb0
enrico <enrico.marquez@duko.com.br>
2024-03-18 21:36:46 -0300

    feat(users): definig create user use case

--- src/api/security/password.py
+++ src/api/security/password.py
@@ -2,8 +2,8 @@ from passlib.context import CryptContext
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_password_hash(password):
+def get_password_hash(password: str):
     return bcrypt_context.hash(password)
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
     return bcrypt_context.verify(plain_password, hashed_password)
--- src/user/domain.py
+++ src/user/domain.py
@@ -1,4 +1,6 @@
 from src.database.connection import DbConnectionHandler
+from src.api.security.password import get_password_hash
+
 
 from .repository import UserRepository
 from .models import UserModel
@@ -11,4 +13,10 @@ class CreateUserUseCase:
         self._payload = payload
 
     async def execute(self):
-        ...
+        await self._repository.create(
+            schemas.User(
+                nickname=self._payload.nickname,
+                email=self._payload.email,
+                password=get_password_hash(self._payload.password)
+            )
+        )
