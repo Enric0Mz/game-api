e17f8bc9c508a8f0b6e0bc3dfe1a5467ce7b306f
enrico <enrico.marquez@duko.com.br>
2024-03-25 20:44:26 -0300

    feat(auth): defining token variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -4,3 +4,9 @@ config = EnvConfig(DotFile(".env-dev", Env.LOCAL))
 
 DB_NAME = config("DB_NAME", str, default="game-api")
 DB_HOST = config("DB_HOST", str)
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRES = 30 # minutes
+REFRESH_TOKEN_EXPIRES = 5 # days
