df64adabd3be77e5abb7ea02e275a001b2354665
enrico <enrico.marquez@duko.com.br>
2024-03-25 20:41:37 -0300

    feat(auth): initializing auth module and defining token model

---
+++ src/auth/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from odmantic import Model
+from odmantic import ObjectId
+from odmantic import Field
+
+
+class Token(Model):
+    user_id: ObjectId = Field(unique=True, index=True)
+    token: str = Field(unique=True)
+    refresh_token: str = Field(unique=True)
+    created_at: datetime
+
