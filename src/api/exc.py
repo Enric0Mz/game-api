b2f3f2f1ca84cffad29343b0fdc42a7f049077bb
enrico <enrico.marquez@duko.com.br>
2024-03-25 21:51:51 -0300

    feat(exc): including not found and invalid password errors

--- src/api/exc.py
+++ src/api/exc.py
@@ -6,3 +6,17 @@ def already_exists_exception(field: str, value: str):
         status_code=status.HTTP_409_CONFLICT, 
         detail=f"value {value} already exists for field {field}"
     )
+
+
+def not_found_exception(field: str, value: str):
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"field {field} not found on {value}"
+    )
+
+
+def incorrect_password_exception():
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect password!"
+    )
