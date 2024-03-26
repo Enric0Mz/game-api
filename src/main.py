5921286ae5866b9dc6898ae057e7fdd73f89d319
Enric0Mz <enricovmarquezz@gmail.com>
2024-03-26 10:28:46 -0300

    feat(cors): adding cores middlewares to integration with front-end

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.events import lifespan
 
@@ -8,6 +9,13 @@ from .routes import router
 def get_application() -> FastAPI:
     _app = FastAPI(title="Game Api", version="0.1.0", debug=True, lifespan=lifespan)
     _app.include_router(router)
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return _app
 
